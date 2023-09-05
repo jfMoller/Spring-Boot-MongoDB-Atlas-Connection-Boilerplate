d3904a4f188201cc19a32188a5a2a3ede4a6bee3
jm <johan.moller040@gmail.com>
2023-09-05 13:34:55 +0200

    first commit

---
+++ src/main/java/com/boilerplate/entity/Product.java
@@ -0,0 +1,47 @@
+package com.boilerplate.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+
+    @Id
+    public String _id;
+
+    private String name;
+
+    private double price;
+
+    private int quantity;
+
+    public Product(String name, double price, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
